6ea146ec3cf2a4ae7957ef066bdee8b040d421d1
donghae <jangdonghae@snu.ac.kr>
2020-11-11 10:30:12 +0900

    directory change

--- rnn_compression_factorization/preprocess.sh
+++ rnn_compression_factorization/preprocess.sh
@@ -5,5 +5,5 @@ mkdir ./src/data
 mv "OpportunityUCIDataset.zip"  ./src/data
 mv "UCI HAR Dataset.zip" ./src/data
 mv "UCI HAR Dataset" ./src/data
-python ./src/preprocess_data.py -i ./src/data/OpportunityUCIDataset.zip -o ./src/oppChallenge_gestures.data
+python ./src/preprocess_data.py -i ./src/data/OpportunityUCIDataset.zip -o oppChallenge_gestures.data
 python ./src/preprocess_HAR2.py
