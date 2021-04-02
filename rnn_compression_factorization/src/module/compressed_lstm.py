a6b6f5c0c96ca4e121b0206e045eb4b11955d9f2
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-04-02 17:28:55 +0900

    Update compressed_lstm.py

--- rnn_compression_factorization/src/module/compressed_lstm.py
+++ rnn_compression_factorization/src/module/compressed_lstm.py
@@ -50,16 +50,18 @@ class myLSTMCell(nn.Module):
     Wi = matmul(W, W_i) and Ui = matmul(U, U_i)
     '''
 
-    def __init__(self, input_size, hidden_size, wRank=None, uRanks=None,g=None, recurrent_init=None,
-                 hidden_init=None,isShuffle=False):
+    def __init__(self, input_size, hidden_size, wRank=None, uRanks=None, recurrent_init=None,
+                 hidden_init=None,g=1,isShuffle=False):
         super(myLSTMCell, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.recurrent_init = recurrent_init
         self.hidden_init = hidden_init
         self.wRank = wRank
-        self.uRanks = uRanks
-        
+        self.uRanks = uRanks 
+        if type(self.uRanks) is list:
+            self.uRanks=uRanks[0]
+        self.g=g
 
         if wRank is None:
             self.W1 = nn.Parameter(
@@ -87,6 +89,7 @@ class myLSTMCell(nn.Module):
             self.U4 = nn.Parameter(
                 0.1 * torch.randn([hidden_size, hidden_size]))
         else:
+            print("uRanks {} type is {}".format(uRanks,type(uRanks)))
             self.U = nn.Parameter(0.1 * torch.randn([hidden_size, uRanks]))
             self.U1 = nn.Parameter(0.1 * torch.randn([uRanks, hidden_size]))
             self.U2 = nn.Parameter(0.1 * torch.randn([uRanks, hidden_size]))
@@ -98,7 +101,7 @@ class myLSTMCell(nn.Module):
         self.bias_c = nn.Parameter(torch.ones([1, hidden_size]))
         self.bias_o = nn.Parameter(torch.ones([1, hidden_size]))
 
-    def forward(self, x, hiddenStates):
+    def forward(self, x, hiddenStates,device):
         (h, c) = hiddenStates
 
         if self.wRank is None:
@@ -144,8 +147,7 @@ class myLSTMCell(nn.Module):
         c_next = f * c + i * c_tilda
         h_next = o * torch.tanh(c_next)
         return h_next, c_next
-
-### 2. Group LSTM cell
+### 3. Group LSTM cell
 class myLSTMGroupCell(nn.Module):
   
     def __init__(self, input_size, hidden_size, wRank=None, uRanks=None,g=2,recurrent_init=None,
@@ -160,20 +162,21 @@ class myLSTMGroupCell(nn.Module):
         self.g=g
         self.isShuffle=isShuffle
         
-        self.W_row=input_size if wRank is None else wRank
-        self.W=None if wRank is None else nn.Parameter(0.1*torch.randn([self.input_size,self.wRank]))
-        self.Ws=[]
-        
+        W_row=input_size if wRank is None else wRank
+        self.W=None if wRank is None else nn.Parameter(0.1*torch.randn([input_size,wRank]))
+        Ws=[]
         #case: only when uRank is not none
         #Ws=[W_f,W_i,W_c,W_o]
         for i in range(4): # of lstm cell gates
-            self.Ws.append(nn.Parameter(0.1*torch.randn([self.W_row,hidden_size])))
+            Ws.append(nn.Parameter(0.1*torch.randn([W_row,hidden_size])))
+        Ws=torch.cat(Ws)
         
-        self.Ug=[[] for i in range(self.g)] # U, UU, UUU, UUUU ... > weight for group 1,2,3,4,...
-        for idx,urank in enumerate(self.uRanks):
-            self.Ug[idx].append(nn.Parameter(0.1*torch.randn([g,int(hidden_size/g),self.uRanks[idx]])))
+        self.Ws=Ws.view(4,W_row,hidden_size)
+        self.Ug=[[] for i in range(g)] # U, UU, UUU, UUUU ... > weight for group 1,2,3,4,...
+        for idx,urank in enumerate(uRanks):
+            self.Ug[idx].append(nn.Parameter(0.1*torch.randn([g,int(hidden_size/g),uRanks[idx]])))
             for i in range(4): #f,i,c,o
-                self.Ug[idx].append(nn.Parameter(0.1*torch.randn([g,self.uRanks[idx],int(hidden_size/g)])))
+                self.Ug[idx].append(nn.Parameter(0.1*torch.randn([g,uRanks[idx],int(hidden_size/g)])))
         
         self.bias_f=nn.Parameter(torch.ones([1,hidden_size]))
         self.bias_i=nn.Parameter(torch.ones([1,hidden_size]))
@@ -187,8 +190,8 @@ class myLSTMGroupCell(nn.Module):
         g_h=g_h.contiguous().view(batch_size,self.hidden_size)
         return g_h
         
-    def forgetgate(self,x,h):
-        wVal_f=torch.matmul(x,self.Ws[0]) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[0])
+    def forgetgate(self,x,h,device):
+        wVal_f=torch.matmul(x,self.Ws[0].to(device)) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[0].to(device))
         batch_size=h.shape[0]
         gidx_list=list(range(self.g))
         hview=h.view(batch_size,self.g,int(self.hidden_size/self.g))
@@ -200,13 +203,13 @@ class myLSTMGroupCell(nn.Module):
             index=gidx_list[g_idx:]+gidx_list[0:g_idx]
             g_h=hview[:,index,:]
             g_h=torch.transpose(g_h,0,1)
-            g_h=torch.bmm(g_h,groupWeight[0])
-            uVal_f+=self.hiddenOperation(g_h,groupWeight[1],batch_size) if self.uRanks[g_idx] > 0 else 0
+            g_h=torch.bmm(g_h,groupWeight[0].to(device))
+            uVal_f+=self.hiddenOperation(g_h,groupWeight[1].to(device),batch_size) if self.uRanks[g_idx] > 0 else 0
         
         return torch.sigmoid(wVal_f+uVal_f+self.bias_f)
     
-    def inputgate(self,x,h):
-        wVal_i=torch.matmul(x,self.Ws[1]) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[1])
+    def inputgate(self,x,h,device):
+        wVal_i=torch.matmul(x,self.Ws[1].to(device)) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[1].to(device))
         batch_size=h.shape[0]
         gidx_list=list(range(self.g))
         hview=h.view(batch_size,self.g,int(self.hidden_size/self.g))
@@ -218,13 +221,13 @@ class myLSTMGroupCell(nn.Module):
             index=gidx_list[g_idx:]+gidx_list[0:g_idx]
             g_h=hview[:,index,:]
             g_h=torch.transpose(g_h,0,1)
-            g_h=torch.bmm(g_h,groupWeight[0])
-            uVal_i+=self.hiddenOperation(g_h,groupWeight[2],batch_size) if self.uRanks[g_idx] > 0 else 0
+            g_h=torch.bmm(g_h,groupWeight[0].to(device))
+            uVal_i+=self.hiddenOperation(g_h,groupWeight[2].to(device),batch_size) if self.uRanks[g_idx] > 0 else 0
         
         return torch.sigmoid(wVal_i+uVal_i+self.bias_i)
         
-    def outgate(self,x,h):
-        wVal_o=torch.matmul(x,self.Ws[2]) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[2])
+    def outgate(self,x,h,device):
+        wVal_o=torch.matmul(x,self.Ws[2].to(device)) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[2].to(device))
         batch_size=h.shape[0]
         gidx_list=list(range(self.g))
         hview=h.view(batch_size,self.g,int(self.hidden_size/self.g))
@@ -236,13 +239,13 @@ class myLSTMGroupCell(nn.Module):
             index=gidx_list[g_idx:]+gidx_list[0:g_idx]
             g_h=hview[:,index,:]
             g_h=torch.transpose(g_h,0,1)
-            g_h=torch.bmm(g_h,groupWeight[0])
-            uVal_o+=self.hiddenOperation(g_h,groupWeight[3],batch_size) if self.uRanks[g_idx] > 0 else 0
+            g_h=torch.bmm(g_h,groupWeight[0].to(device))
+            uVal_o+=self.hiddenOperation(g_h,groupWeight[3].to(device),batch_size) if self.uRanks[g_idx] > 0 else 0
 
         return torch.sigmoid(wVal_o+uVal_o+self.bias_o)   
         
-    def gate_gate(self,x,h):
-        wVal_g=torch.matmul(x,self.Ws[3]) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[3])
+    def gate_gate(self,x,h,device):
+        wVal_g=torch.matmul(x,self.Ws[3].to(device)) if self.wRank is None else torch.matmul(torch.matmul(x,self.W),self.Ws[3].to(device))
         batch_size=h.shape[0]
         gidx_list=list(range(self.g))
         hview=h.view(batch_size,self.g,int(self.hidden_size/self.g))
@@ -254,18 +257,22 @@ class myLSTMGroupCell(nn.Module):
             index=gidx_list[g_idx:]+gidx_list[0:g_idx]
             g_h=hview[:,index,:]
             g_h=torch.transpose(g_h,0,1)
-            g_h=torch.bmm(g_h,groupWeight[0])
-            uVal_g+=self.hiddenOperation(g_h,groupWeight[4],batch_size) if self.uRanks[g_idx] > 0 else 0
+            g_h=torch.bmm(g_h,groupWeight[0].to(device))
+            uVal_g+=self.hiddenOperation(g_h,groupWeight[4].to(device),batch_size) if self.uRanks[g_idx] > 0 else 0
         
         return torch.tanh(wVal_g+uVal_g+self.bias_c)   
-        
-    def forward(self,x,hiddenStates):
+
+    def shuffle(self,x):
+        perm=torch.randperm(x.shape[-1])
+        return x[:,perm]   
+
+    def forward(self,x,hiddenStates,device):
         (h,c)=hiddenStates
 
-        val_f=self.forgetgate(x,h)
-        val_i=self.inputgate(x,h)
-        val_o=self.outgate(x,h)s
-        val_c=self.gate_gate(x,h)
+        val_f=self.forgetgate(x,h,device)
+        val_i=self.inputgate(x,h,device)
+        val_o=self.outgate(x,h,device)
+        val_c=self.gate_gate(x,h,device)
         
         c_next=val_f*c+val_i*val_c
         h_next=val_o*torch.tanh(c_next)
@@ -275,7 +282,6 @@ class myLSTMGroupCell(nn.Module):
         
         return h_next,c_next
 
-### 3. LSTM network 
 class myLSTM(nn.Module):
     def __init__(self, input_size, hidden_layer_sizes=[32, 32], batch_first=True, recurrent_inits=None,
                  hidden_inits=None, wRank=None, uRanks=None, recurrent=False,cell=myLSTMCell,g=None,isShuffle=False, **kwargs):
@@ -288,11 +294,14 @@ class myLSTM(nn.Module):
         self.isShuffle=isShuffle
         self.g=g
         self.cell=cell
-        print("cell:{}".format(self.cell))
-        print("recurrent:{}\n".format(recurrent))
-        print("start training with wRank:{}".format(wRank))
-        print("start training with uRanks:{}".format(uRanks))
+        if g is None or g <2:
+            self.uRanks=uRanks[0] if type(uRanks) is list else uRanks
+        print("#1.cell:{}".format(self.cell))
+        print("#2.recurrent:{}".format(recurrent))
+        print("#start training with wRank:{}".format(wRank))
+        print("#start training with uRanks:{}".format(uRanks))
 
+        
         if batch_first:
             self.time_index = 1
             self.batch_index = 0
@@ -334,7 +343,7 @@ class myLSTM(nn.Module):
             x_time = torch.unbind(x, time_index)
             seqlen = len(x_time)
             for t in range(seqlen):
-                h, c = cell(x_time[t], (h, c))
+                h, c = cell(x_time[t], (h, c),self.device)
                 outputs.append(h)
             x = torch.stack(outputs, time_index)
             # x=torch.cat(outputs, -1)
