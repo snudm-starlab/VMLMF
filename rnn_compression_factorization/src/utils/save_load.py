e9540d876252cde8ef4645fdf6f4e844b7fb76d0
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-06-15 21:49:13 +0900

    Update save_load.py

--- rnn_compression_factorization/src/utils/save_load.py
+++ rnn_compression_factorization/src/utils/save_load.py
@@ -1,16 +1,3 @@
-## train 과정에서 
-"""
-path="./weights/"
-name="comp_vmmodel_w{}_u{}".format(args.wRank,args.uRanks)
-state_dict=torch.load(path+name+".pkl")
-model.load_state_dict(state_dict)
-print_model_parm_nums(model)
-print_model_parm_names(model)
-total_ops=count_lstm(model,args.max_steps,args.batch_size)
-total_ops+=count_linear(model,10)
-print("+ total_ops:",total_ops)
-"""
-#
 import os
 import sys
 import torch
