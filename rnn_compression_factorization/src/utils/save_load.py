ca0eabde8ead7c6c66f8a2eb05129edbb7ca9625
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-06-15 21:48:52 +0900

    update save_load.py
    add some details - make dir, check whether file exists

--- rnn_compression_factorization/src/utils/save_load.py
+++ rnn_compression_factorization/src/utils/save_load.py
@@ -11,13 +11,28 @@ total_ops+=count_linear(model,10)
 print("+ total_ops:",total_ops)
 """
 #
-
+import os
+import sys
 import torch
 
-def save_model(model,path="./weights",name="comp_vmmodel_wNone_uNone"):
+def save_model(model,path="./trained_model/",name="comp_vmmodel_wNone_uNone"):
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
     torch.save(model.state_dict(),path+name+".pkl")
+    print("model saved in %s"%(path+name+".pkl"))
 
-def load_model(model,path="./weights",name="comp_vmmodel_wNone_uNone"):
-    state_dict=torch.load(path+name+".pkl")
-    model.load_state_dict(state_dict)
+def load_model(model,path="./trained_model/",name="comp_vmmodel_wNone_uNone"):
+    
+    file=path+name+".pkl"
+    
+    if os.path.exists(file):
+        state_dict=torch.load(file)
+        model.load_state_dict(state_dict)
+        print("model restored from %s"%(file))
+    else:
+        print(name+'pkl does not exists.')
+        print("Testing can only be done when the trained model exists.")
+        sys.exit()
+        
     return model
