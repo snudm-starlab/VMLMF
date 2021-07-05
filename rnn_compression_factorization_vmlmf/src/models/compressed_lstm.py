a031e1581d61eb5aad0e28832704580a51652321
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-07-05 10:45:05 +0900

    Update contact information

--- rnn_compression_factorization_vmlmf/src/models/compressed_lstm.py
+++ rnn_compression_factorization_vmlmf/src/models/compressed_lstm.py
@@ -1,3 +1,18 @@
+################################################################################
+# Starlab RNN-compression with factorization method : Lowrank Factorization with vector-multiplication
+#
+# Author: Hyojin Jeon (tarahjjeon@snu.ac.kr), Seoul National University
+#         U Kang (ukang@snu.ac.kr), Seoul National University
+#
+# Version : 1.0
+# Date : Jul 08, 2021
+# Main Contact: Hyojin Jeon
+#
+# This software is free of charge under research purposes.
+# For commercial purposes, please contact the authors.
+#
+################################################################################
+
 import torch
 from torch.nn import Parameter, ParameterList
 import torch.nn as nn
