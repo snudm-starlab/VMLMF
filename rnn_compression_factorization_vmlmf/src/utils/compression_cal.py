40bc0fb60ea923b2d9f908432ba9ed52ae5fa785
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-07-05 10:55:28 +0900

    Create compression_cal.py

---
+++ rnn_compression_factorization_vmlmf/src/utils/compression_cal.py
@@ -0,0 +1,14 @@
+################################################################################
+# Starlab RNN-compression with factorization method : Lowrank Factorization with vector-multiplication
+#
+# Author: Hyojin Jeon (tarahjjeon@snu.ac.kr), Seoul National University
+#         U Kang (ukang@snu.ac.kr), Seoul National University
+#
+# Version : 1.0
+# Date : Jul 08, 2021
+# Main Contact: Hyojin Jeon
+#
+# This software is free of charge under research purposes.
+# For commercial purposes, please contact the authors.
+#
+################################################################################
