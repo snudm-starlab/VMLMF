52c3bb13d2dfeb768bb0ee1b12a0c83c658dcd49
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-07-05 10:56:53 +0900

    Update OPP_dataloader.py

--- rnn_compression_factorization_vmlmf/src/utils/OPP_dataloader.py
+++ rnn_compression_factorization_vmlmf/src/utils/OPP_dataloader.py
@@ -12,3 +12,36 @@
 # For commercial purposes, please contact the authors.
 #
 ################################################################################
+from torch.utils.data import Dataset, DataLoader
+import torch
+import numpy as np
+
+class CustomDataset(Dataset):
+    def __init__(self, data_path, mode):
+        import numpy as np
+        self.X = np.load((data_path + '/' + 'X_' + mode + '.npy'))
+        self.y = np.load((data_path + '/' + 'y_' + mode + '.npy'))
+        #print(self.X.shape)
+        #print(self.y.shape)
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+
+    def __len__(self):
+        return len(self.X)
+
+
+def HAR_dataloader(batch_size, dataset_folder='./data/smalldata'):
+    dataset_train = CustomDataset(dataset_folder, 'train')
+    train_loader = DataLoader(dataset=dataset_train,
+                              batch_size=batch_size,
+                              shuffle=True, drop_last=True
+                              )
+
+    dataset_test = CustomDataset(dataset_folder, 'test')
+    test_loader = DataLoader(dataset=dataset_test,
+                             batch_size=batch_size,
+                             shuffle=False
+                             )
+
+    return (train_loader, test_loader)
