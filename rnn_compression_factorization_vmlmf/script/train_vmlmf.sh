bdbb95404da16b59ec186992a7ca7698bbdad830
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-07-20 09:37:39 +0900

    Update train_vmlmf.sh

--- rnn_compression_factorization_vmlmf/script/train_vmlmf.sh
+++ rnn_compression_factorization_vmlmf/script/train_vmlmf.sh
@@ -1,9 +1,5 @@
 cd ./src/
-echo "Start training vanilla_lstm"
-python ./train_test/main.py --model mylstm --layer_sizes 180 -train
-echo "Start training..."
-python ./train_test/main.py --model vmmodel --layer_sizes 180 --wRank 8 --uRanks 6 -train --seed 4 
-echo "Test baseline model.."
-python ./train_test/main.py --model mylstm --layer_sizes 180
-echo "Test compressed model.."
-python ./train_test/main.py --model vmmodel --layer_sizes 180 --wRank 8 --uRanks 6 --seed 4
+echo "Start training compressed model..."
+python ./train_test/main_total.py --model vmmodel_final --layer_sizes 180 --wRank 8 --uRanks 4 -train --seed 3 --gpu_id 2
+echo "Test Baseline model.."
+python ./train_test/main.py --model mylstm --layer_sizes 180 --seed 3
