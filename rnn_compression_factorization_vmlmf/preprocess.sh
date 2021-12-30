7f470b4c760dbbddb51a39200bcfd2694c3d883c
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-12-30 14:50:20 +0900

    Update preprocess.sh

--- rnn_compression_factorization_vmlmf/preprocess.sh
+++ rnn_compression_factorization_vmlmf/preprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir ./src/data/smalldata
-python ./src/download_dataset.py
-mv "OpportunityUCIDataset.zip"  ./src/data/smalldata
+mkdir ./src/data/opp
+python3 ./src/download_dataset.py
+mv "OpportunityUCIDataset.zip"  ./src/data/
 mv "UCI HAR Dataset.zip" ./src/data
 mv "UCI HAR Dataset" ./src/data
-python ./src/preprocess_Opportunity77.py -i ./src/data/smalldata/OpportunityUCIDataset.zip -o oppChallenge_gestures.data
+python3 ./src/preprocess_opp.py -i ./src/data/OpportunityUCIDataset.zip -o oppChallenge_gestures.data
